8767fea1ee9a87963320c43b077ed544f5e21932
Sean Budhram <sbudhram@carlisle-etcetera.com>
2018-10-02 16:13:38 -0400

    first commit

---
+++ real-world-vue/src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+              {{ msg }}
+          </h1>
+          <h2 class="subtitle">
+            Welcome to learning Vue. We are finally on the way.
+          </h2>
+        </div>
+       </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    msg: String,
+  },
+};
+</script>
---
+++ real-world-vue/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Hero msg="Welcome to Vuejs"></Hero>
+    <HelloWorld msg="Lorem Ipsum is simply dummy text of the printing and
+     typesetting industry." ></HelloWorld>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Hero from '@/components/Hero.vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Hero,
+    HelloWorld,
+  },
+};
+</script>
